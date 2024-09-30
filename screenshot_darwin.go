65bc283eb8bc74ec3b8346ee332e1ed0814f79e0
Iain MacDonald <iain@smartretail.co>
2024-09-30 16:45:57 +0800

    Replace broken Darwin 15 implementation with dummy method

--- screenshot_darwin.go
+++ screenshot_darwin.go
@@ -7,9 +7,6 @@ import (
 	// #include <CoreFoundation/CoreFoundation.h>
 	"C"
 	"image"
-	"reflect"
-	"unsafe"
-	"math"
 )
 
 func ScreenRect() (image.Rectangle, error) {
@@ -28,26 +25,10 @@ func CaptureScreen() (*image.RGBA, error) {
 }
 
 func CaptureRect(rect image.Rectangle) (*image.RGBA, error) {
-	displayID := C.CGMainDisplayID()
-	width := int(math.Ceil(float64(C.CGDisplayPixelsWide(displayID))/16)*16)
-	rawData := C.CGDataProviderCopyData(C.CGImageGetDataProvider(C.CGDisplayCreateImage(displayID)))
-
-	length := int(C.CFDataGetLength(rawData))
-	ptr := unsafe.Pointer(C.CFDataGetBytePtr(rawData))
-
-	var slice []byte
-	hdrp := (*reflect.SliceHeader)(unsafe.Pointer(&slice))
-	hdrp.Data = uintptr(ptr)
-	hdrp.Len = length
-	hdrp.Cap = length
+	width := 1024
+	length := 0
 
 	imageBytes := make([]byte, length)
-
-	for i := 0; i < length; i += 4 {
-		imageBytes[i], imageBytes[i+2], imageBytes[i+1], imageBytes[i+3] = slice[i+2], slice[i], slice[i+1], slice[i+3]
-	}
-
-	C.CFRelease(C.CFTypeRef(rawData))
 
 	img := &image.RGBA{Pix: imageBytes, Stride: 4 * width, Rect: rect}
 	return img, nil
